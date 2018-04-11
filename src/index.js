b84e8231e56046d765098e842f354fe9a828c532
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-04-11 11:57:06 +0100

    enabling redux devtools from chrome store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import {
+    createStore
+} from 'redux';
 //this is the default export from the reducers folder could be called anything 'reducer' in this instance
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 console.log(reducer);
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
 registerServiceWorker();
