19f704d92e72c152f44024d75348e893cbade247
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-04-11 12:13:46 +0100

    hook up the actions to the react component

--- src/index.js
+++ src/index.js
@@ -13,6 +13,5 @@ const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-console.log(reducer);
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render( < App store={store} / > , document.getElementById('root'));
 registerServiceWorker();
